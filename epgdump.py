e86ad91dcfb5507cd1cc843f1636960ab92d60da
ita mt <6236260+itamt@users.noreply.github.com>
2019-10-01 15:10:57 +0900

    if __name__ == '__main__'

--- epgdump.py
+++ epgdump.py
@@ -26,81 +26,87 @@ def usage():
 ''', file=sys.stderr)
 
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:], 'hbsc:dfi:o:p:e',
-                               ['help', 'bs', 'cs', 'channel-id=', 'debug', 'format', 'input=', 'output=',
-                                'print-time=', 'event-id'])
-except IndexError as xxx_todo_changeme:
-    getopt.GetoptError = xxx_todo_changeme
-    usage()
-    sys.exit(1)
-
-channel_id = None
-input_file = None
-output_file = None
-pretty_print = False
-debug = False
-b_type = TYPE_DEGITAL
-transport_stream_id = None
-service_id = None
-event_id = None
-output_eid = False
-for o, a in opts:
-    if o in ('-h', '--help'):
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, 'hbsc:dfi:o:p:e',
+                                   ['help', 'bs', 'cs', 'channel-id=', 'debug', 'format', 'input=', 'output=',
+                                    'print-time=', 'event-id'])
+    except IndexError as xxx_todo_changeme:
+        getopt.GetoptError = xxx_todo_changeme
         usage()
-        sys.exit(0)
-    elif o in ('-b', '--bs'):
-        b_type = TYPE_BS
-    elif o in ('-s', '--cs'):
-        b_type = TYPE_CS
-    elif o in ('-c', '--channel-id'):
-        channel_id = a
-    elif o in ('-d', '--debug'):
-        debug = True
-    elif o in ('-f', '--format'):
-        pretty_print = True
-    elif o in ('-i', '--input'):
-        input_file = a
-    elif o in ('-o', '--output'):
-        output_file = a
-    elif o in ('-p', '--print-time'):
-        # 検索用オプション
-        arr = a.split(':')
-        transport_stream_id = int(arr[0])
-        service_id = int(arr[1])
-        event_id = int(arr[2])
-    elif o in ('-e', '--event-id'):
-        output_eid = True
-
-if service_id == None and (
-        (b_type == TYPE_DEGITAL and channel_id == None) or input_file == None or output_file == None):
-    usage()
-    sys.exit(1)
-elif input_file == None:
-    usage()
-    sys.exit(1)
-
-tsfile = TransportStreamFile(input_file, 'rb')
-(service, events) = parse_ts(b_type, tsfile, debug)
-tsfile.close()
-if service_id == None:
-    # xmp出力
-    xmltv.create_xml(b_type, channel_id, service, events, output_file, pretty_print, output_eid)
-else:
-    # 検索用オプション指定時の動作
-    start_time = None
-    end_time = None
-    for event in events:
-        if (event.transport_stream_id == transport_stream_id and
-                event.service_id == service_id and
-                event.event_id == event_id):
-            start_time = event.start_time
-            end_time = event.start_time + event.duration
-            break
-    if start_time == None:
-        print(
-            "not found: transport_stream_id=%d service_id=%d event_id=%d" % (transport_stream_id, service_id, event_id),
-            file=sys.stderr)
         sys.exit(1)
+
+    channel_id = None
+    input_file = None
+    output_file = None
+    pretty_print = False
+    debug = False
+    b_type = TYPE_DEGITAL
+    transport_stream_id = None
+    service_id = None
+    event_id = None
+    output_eid = False
+    for o, a in opts:
+        if o in ('-h', '--help'):
+            usage()
+            sys.exit(0)
+        elif o in ('-b', '--bs'):
+            b_type = TYPE_BS
+        elif o in ('-s', '--cs'):
+            b_type = TYPE_CS
+        elif o in ('-c', '--channel-id'):
+            channel_id = a
+        elif o in ('-d', '--debug'):
+            debug = True
+        elif o in ('-f', '--format'):
+            pretty_print = True
+        elif o in ('-i', '--input'):
+            input_file = a
+        elif o in ('-o', '--output'):
+            output_file = a
+        elif o in ('-p', '--print-time'):
+            # 検索用オプション
+            arr = a.split(':')
+            transport_stream_id = int(arr[0])
+            service_id = int(arr[1])
+            event_id = int(arr[2])
+        elif o in ('-e', '--event-id'):
+            output_eid = True
+
+    if service_id == None and (
+            (b_type == TYPE_DEGITAL and channel_id == None) or input_file == None or output_file == None):
+        usage()
+        sys.exit(1)
+    elif input_file == None:
+        usage()
+        sys.exit(1)
+
+    tsfile = TransportStreamFile(input_file, 'rb')
+    (service, events) = parse_ts(b_type, tsfile, debug)
+    tsfile.close()
+    if service_id == None:
+        # xmp出力
+        xmltv.create_xml(b_type, channel_id, service, events, output_file, pretty_print, output_eid)
     else:
-        print(int(time.mktime(start_time.timetuple())), int(time.mktime(end_time.timetuple())))
+        # 検索用オプション指定時の動作
+        start_time = None
+        end_time = None
+        for event in events:
+            if (event.transport_stream_id == transport_stream_id and
+                    event.service_id == service_id and
+                    event.event_id == event_id):
+                start_time = event.start_time
+                end_time = event.start_time + event.duration
+                break
+        if start_time == None:
+            print(
+                "not found: transport_stream_id=%d service_id=%d event_id=%d" % (
+                transport_stream_id, service_id, event_id),
+                file=sys.stderr)
+            sys.exit(1)
+        else:
+            print(int(time.mktime(start_time.timetuple())), int(time.mktime(end_time.timetuple())))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
