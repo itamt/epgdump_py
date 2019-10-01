9faae95f2b975484ddf9b5cbce7365e0895f4724
ita mt <6236260+itamt@users.noreply.github.com>
2019-10-01 18:47:34 +0900

    引数の処理を getopt から argparse に変更

--- epgdump.py
+++ epgdump.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import sys
-import getopt
 import time
-from typing import List
+import argparse
+from typing import List, Optional
 
 import xmltv
 from parser_ import TransportStreamFile, parse_ts
@@ -12,81 +12,71 @@ from constant import (
 )
 
 
-def usage():
-    print('''USAGE: epgdump_py -c CHANNEL_ID -i INPUT_FILE -o OUTPUT_FILE
-       epgdump_py -b -i INPUT_FILE -o OUTPUT_FILE
-       epgdump_py -s -i INPUT_FILE -o OUTPUT_FILE
-       epgdump_py [-b|-s] -p TRANSPORT_STREAM_ID:SERVICE_ID:EVENT_ID -i INPUT_FILE
-  -h, --help        print help message
-  -b, --bs          input file is BS channel
-  -s, --cs          input file is CS channel
-  -c, --channel-id  specify channel identifier
-  -d, --debug       parse all ts packet
-  -f, --format      format xml
-  -i, --input       specify ts file
-  -o, --output      specify xml file
-  -p, --print-time  print start time, and end time of specified id
-  -e, --event-id    output transport_stream_id, servece_id and event_id
-''', file=sys.stderr)
+def create_argparser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description="""example:
+    epgdump.py -c CHANNEL_ID -i INPUT_FILE -o OUTPUT_FILE
+    epgdump.py -b -i INPUT_FILE -o OUTPUT_FILE
+    epgdump.py -s -i INPUT_FILE -o OUTPUT_FILE
+    epgdump.py [-b|-s] -p TRANSPORT_STREAM_ID:SERVICE_ID:EVENT_ID -i INPUT_FILE""",
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.print_usage = parser.print_help  # overwrite usage by full help
+    group = parser.add_mutually_exclusive_group(required=False)
+    group.add_argument('-b', '--bs', action='store_const', dest='b_type', const=TYPE_BS,
+                       help='input file is BS channel')
+    group.add_argument('-s', '--cs', action='store_const', dest='b_type', const=TYPE_CS,
+                       help='input file is CS channel')
+    parser.add_argument('-c', '--channel-id', type=str,
+                        help='specify channel identifier')
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='parse all ts packet')
+    parser.add_argument('-f', '--format', action='store_true',
+                        help='format xml')
+    parser.add_argument('-i', '--input', type=argparse.FileType('r'),
+                        help='specify ts file', metavar='INPUT_FILE')
+    parser.add_argument('-o', '--output', type=str,
+                        help='specify xml file', metavar='OUTPUT_FILE')
+    parser.add_argument('-p', '--print-time', type=str,
+                        help='print start time, and end time of specified id',
+                        metavar='TRANSPORT_STREAM_ID:SERVICE_ID:EVENT_ID')
+    parser.add_argument('-e', '--event-id', action='store_true',
+                        help='output transport_stream_id, servece_id and event_id')
+    return parser
 
 
 def main(argv: List[str]):
-    try:
-        opts, args = getopt.getopt(argv, 'hbsc:dfi:o:p:e',
-                                   ['help', 'bs', 'cs', 'channel-id=', 'debug', 'format', 'input=', 'output=',
-                                    'print-time=', 'event-id'])
-    except IndexError as xxx_todo_changeme:
-        getopt.GetoptError = xxx_todo_changeme
-        usage()
-        sys.exit(1)
+    argparser = create_argparser()
+    args = argparser.parse_args(argv)
 
-    channel_id = None
-    input_file = None
-    output_file = None
-    pretty_print = False
-    debug = False
-    b_type = TYPE_DEGITAL
-    transport_stream_id = None
-    service_id = None
-    event_id = None
-    output_eid = False
-    for o, a in opts:
-        if o in ('-h', '--help'):
-            usage()
-            sys.exit(0)
-        elif o in ('-b', '--bs'):
-            b_type = TYPE_BS
-        elif o in ('-s', '--cs'):
-            b_type = TYPE_CS
-        elif o in ('-c', '--channel-id'):
-            channel_id = a
-        elif o in ('-d', '--debug'):
-            debug = True
-        elif o in ('-f', '--format'):
-            pretty_print = True
-        elif o in ('-i', '--input'):
-            input_file = a
-        elif o in ('-o', '--output'):
-            output_file = a
-        elif o in ('-p', '--print-time'):
-            # 検索用オプション
-            arr = a.split(':')
-            transport_stream_id = int(arr[0])
-            service_id = int(arr[1])
-            event_id = int(arr[2])
-        elif o in ('-e', '--event-id'):
-            output_eid = True
+    channel_id: str = args.channel_id
+    input_file: str = args.input.name if args.input else None
+    output_file: str = args.output
+    pretty_print: bool = args.format
+    debug: bool = args.debug
+    b_type: str = args.b_type or TYPE_DEGITAL
+    transport_stream_id: Optional[int] = None
+    service_id: Optional[int] = None
+    event_id: Optional[int] = None
+    output_eid: bool = args.event_id
 
-    if service_id is None and (
-            (b_type == TYPE_DEGITAL and channel_id is None) or input_file is None or output_file is None):
-        usage()
+    if args.print_time:
+        # 検索用オプション
+        arr = args.print_time.split(':')
+        transport_stream_id = int(arr[0])
+        service_id = int(arr[1])
+        event_id = int(arr[2])
+
+    if not args.print_time and \
+            (b_type == TYPE_DEGITAL and channel_id is None) \
+            or input_file is None \
+            or output_file is None:
+        argparser.print_help()
         sys.exit(1)
     elif input_file is None:
-        usage()
+        argparser.print_help()
         sys.exit(1)
 
     tsfile = TransportStreamFile(input_file, 'rb')
-    (service, events) = parse_ts(b_type, tsfile, debug)
+    service, events = parse_ts(b_type, tsfile, debug)
     tsfile.close()
     if service_id is None:
         # xmp出力
@@ -105,7 +95,7 @@ def main(argv: List[str]):
         if start_time is None:
             print(
                 "not found: transport_stream_id=%d service_id=%d event_id=%d" % (
-                transport_stream_id, service_id, event_id),
+                    transport_stream_id, service_id, event_id),
                 file=sys.stderr)
             sys.exit(1)
         else:
